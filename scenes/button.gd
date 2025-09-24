127d978ca0a4fc54c7e625fabb12a0632f2b2979
Benur21 <you@example.com>
2025-09-24 21:26:11 +0100

    Add 2nd house part 1

--- scenes/button.gd
+++ scenes/button.gd
@@ -1,10 +1,11 @@
 extends Button
 
 var house_building = preload("res://scenes/house_building.tscn")
-@export var selected = false
+@export var type = "house1"
 
 func _pressed() -> void:
-	if not selected:
+	if not get_tree().root.has_node("World/house_building"):
 		var instance = house_building.instantiate()
+		instance.type = type
 		self.get_parent().add_child(instance)
-		selected = true
+		instance.selected = true
--- scenes/house_building.gd
+++ scenes/house_building.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-# "Obter size Node2D"
+@export var type = "house1"
+@export var selected = false
+
+# GPT "Obter size Node2D"
 func get_total_bounds() -> Rect2:
 	var rect := Rect2()
 	var first := true
 	
 	for child in get_children():
-		if child is TileMapLayer:
+		if child is TileMapLayer and child.enabled:
 			var used_rect = child.get_used_rect() # em coords de tiles
 			var cell_size = child.tile_set.tile_size
 			
@@ -33,13 +36,30 @@ func get_total_bounds() -> Rect2:
 	return rect
 
 
+func round_to_nearest_multiple(x: float, n: float) -> float:
+	return int(roundf(x / n )) * n
+
+func _ready() -> void:
+	$background1.enabled = false
+	$house1.enabled = false
+	$background2.enabled = false
+	$house2.enabled = false
+	if type == "house1":
+		$background1.enabled = true
+		$house1.enabled = true
+	elif type == "house2":
+		$background2.enabled = true
+		$house2.enabled = true
+		
 func _physics_process(delta: float) -> void:
-	self.position.x = get_global_mouse_position().x - get_total_bounds().size.x/2
-	self.position.y = get_global_mouse_position().y - get_total_bounds().size.y/2
+	const tile_size := 16*2.5;
+	self.position.x = round_to_nearest_multiple( get_global_mouse_position().x \
+		- get_total_bounds().size.x/2, tile_size )
+	self.position.y = round_to_nearest_multiple( get_global_mouse_position().y \
+		- get_total_bounds().size.y/2, tile_size )
 
 	if Input.is_action_just_pressed("ui_cancel") or Input.is_action_just_pressed("click_right"):
-		# Por path absoluto
-		get_tree().root.get_node("World/Button").selected = false
+		selected = false
 		queue_free() # Remove o node da tree
 		
 		
